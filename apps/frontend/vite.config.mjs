8900f1db698cc9897d181b6cb70e9b2a6498edac
KareemYasser101 <152732674+KareemYasser101@users.noreply.github.com>
2025-05-24 01:34:53 +0300

    solved deployment bug

--- apps/frontend/vite.config.mjs
+++ apps/frontend/vite.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   port: 3000,
   proxy: {
     "/api/trpc": {
-      target: process.env.VITE_API_URL || "http://localhost:4000",
+      target: process.env.VITE_API_URL || "http://localhost:8080",
       changeOrigin: true,
     },
   },
